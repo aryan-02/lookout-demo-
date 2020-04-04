34584f7f60069ac85103efff729ab5f6fea6210a
Aryan Mediratta <aryan_02@outlook.com>
2020-04-04 12:25:42 +0530

    Async/Await

--- routes/forms.js
+++ routes/forms.js
@@ -1,34 +1,54 @@
 const router = require('express').Router();
 let Com = require('../models/form.model');
 
-router.route('/').get((req, res) => {
-    Com.find({category: req.body.category})
-        .then(coms =>  {
-              let S = coms.length;
-              let sum=0;
-              for(let i =0;i<S;i++){
-                    sum += Number(coms[i].index); 
-              }
-              sum= sum/S;   
-              res.json({sum});
-              
-        })
-        .catch(err => res.statusCode(400).json('Error: ' + err));
+router.route('/').get( async (req, res) => {
+	try {
+		const coms = await Com.find({category: req.body.category});
+		const S = coms.length;
+		let sum=0;
+		for(let i =0;i<S;i++){
+			sum += Number(coms[i].index);
+		}
+		const average = sum/S;
+		res.json({average});
+	} catch(err) {
+		res.statusCode(400).json('Error: ' + err);
+	}
+
+	/* Com.find({category: req.body.category})
+		.then(coms =>  {
+			let S = coms.length;
+			let sum=0;
+			for(let i =0;i<S;i++){
+				sum += Number(coms[i].index);
+			}
+			sum= sum/S;
+			res.json({sum});
+
+		})
+		.catch(err => res.statusCode(400).json('Error: ' + err)); */
 });
 
 
 
-router.route('/add').post((req,res) => {
-    const index = Number(req.body.index);
-    const category = req.body.category;
-    const newCom = new Com({
-        index,
-        category,
-    });
+router.route('/add').post( async (req, res) => {
+	const index = Number(req.body.index);
+	const category = req.body.category;
+	const newCom = new Com({
+		index,
+		category,
+	});
 
-    newCom.save()
-           .then(() => res.json(`${category} added!`))
-           .catch(err => res.statusCode(400).json('Error: ' + err));
+	try {
+		const savedCom = await newCom.save();
+		res.json(`${category} added!`);
+	} catch(err) {
+		res.statusCode(400).json('Error: ' + err);
+	}
+
+	/* newCom.save()
+		   .then(() => res.json(`${category} added!`))
+		   .catch(err => res.statusCode(400).json('Error: ' + err)); */
 });
 
 
--- routes/lqis.js
+++ routes/lqis.js
@@ -1,8 +1,21 @@
 const router = require('express').Router();
 let Form = require('../models/form.model');
 
-router.route('/').get((req, res) => {
-    Form.find()
+router.route('/').get( async (req, res) => {
+    try {
+        const coms = await Form.find();
+        const S = coms.length;
+        let sum = 0;
+        for(let i = 0; i < S; i++)
+        {
+            sum += Number(coms[i].index);
+        }
+        const average = sum / S;
+        res.json({avg: average, coms});
+    } catch(err) {
+        res.statusCode(400).json('Error: ' + err);
+    }
+    /* Form.find()
         .then(coms =>  {
               let S = coms.length;
               let sum=0;
@@ -13,7 +26,7 @@ router.route('/').get((req, res) => {
             res.json({avg:sum, coms});
       
         })
-        .catch(err => res.statusCode(400).json('Error: ' + err));
+        .catch(err => res.statusCode(400).json('Error: ' + err)); */
 });
 
 module.exports = router;
