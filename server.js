4d731c9bb59df5260dd10bd909b414a4fadd0e74
devsbdev <63121945+devsbdev@users.noreply.github.com>
2020-04-04 11:20:57 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -5,38 +5,29 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+const port = process.env.PORT || 5000;
 
 app.use(cors());
-
 app.use(express.json());
 
-
 const uri = process.env.ATLAS_URI;
-//const db = require("./config/keys").mongoURI;
-
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true  }
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}
 );
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
 
+
 const usersRouter = require('./routes/users');
- //const comsRouter = require('./routes/coms');
-// const healthsRouter = require('./routes/healths');
-// const crowdsRouter = require('./routes/crowds');
-// const hygienesRouter = require('./routes/hygienes');
-// const lqisRouter = require('./routes/lqis');
-// const formsRouter = require('./routes/forms');
+const formsRouter = require('./routes/forms');
+const lqisRouter = require('./routes/lqis');
+
 
 app.use('/users', usersRouter);
- //app.use('/coms', comsRouter)
-// app.use('/healths', healthsRouter)
-// app.use('/crowds', crowdsRouter)
-// app.use('/hygienes', hygienesRouter)
-// app.use('/lqis', lqisRouter)
-// app.use('/forms', formsRouter)
+app.use('/forms', formsRouter);
+app.use('/lqis', lqisRouter);
+
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
